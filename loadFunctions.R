87c1c051b3d235bee301394ffc7bd72de0b7b48b
Davide Vidotto <34507244+davidevdt@users.noreply.github.com>
2019-08-12 15:36:57 +0200

    Load files necessary to run the simulations

---
+++ loadFunctions.R
@@ -0,0 +1,9 @@
+source("https://raw.githubusercontent.com/davidevdt/bayespcaSim/master/AuxFuncs/auxfunctions.R")
+source("https://raw.githubusercontent.com/davidevdt/bayespcaSim/master/DataGeneratingFunctions/datageneration.R")
+source("https://raw.githubusercontent.com/davidevdt/bayespcaSim/master/DataGeneratingFunctions/genLoading.R")
+source("https://raw.githubusercontent.com/davidevdt/bayespcaSim/master/DataGeneratingFunctions/genZeroMat.R")
+source("https://raw.githubusercontent.com/davidevdt/bayespcaSim/master/spcaFunctions/eigencv.R")
+source("https://raw.githubusercontent.com/davidevdt/bayespcaSim/master/SimulationFunctions/estimateModels.R")
+source("https://raw.githubusercontent.com/davidevdt/bayespcaSim/master/SimulationFunctions/runSim.R")
+
+
